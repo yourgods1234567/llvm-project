b71f2f5e7a485089ef3d757d7d22de9bb187e1ed
Kostas Alvertis <konstantinos.alvertis@gmail.com>
2025-11-14 21:56:39 +0000

    [PPC][BOLT] Skip anonymous non-allocatable sections to avoid invalid sh_name offsets
    Anonymous backups such as .bolt.org.text were getting section headers despite not being added to .shstrtab, causing a crash in getOffset(). This patch excludes anonymous non-allocatable sections when emitting headers, preventing invalid sh_name assignments.

--- bolt/lib/Rewrite/RewriteInstance.cpp
+++ bolt/lib/Rewrite/RewriteInstance.cpp
@@ -5432,6 +5432,9 @@ RewriteInstance::getOutputSections(ELFObjectFile<ELFT> *File,
     BinarySection *BinSec = BC->getSectionForSectionRef(SecRef);
     assert(BinSec && "Matching BinarySection should exist.");
 
+    if(BinSec->isAnonymous())
+      continue;
+
     ELFShdrTy NewSection = Section;
     NewSection.sh_offset = BinSec->getOutputFileOffset();
     NewSection.sh_size = BinSec->getOutputSize();
@@ -5447,6 +5450,9 @@ RewriteInstance::getOutputSections(ELFObjectFile<ELFT> *File,
   // Create entries for new non-allocatable sections.
   for (BinarySection &Section : BC->nonAllocatableSections()) {
     if (Section.getOutputFileOffset() <= LastFileOffset)
+      continue;
+
+    if( Section.isAnonymous())
       continue;
 
     if (opts::Verbosity >= 1)
