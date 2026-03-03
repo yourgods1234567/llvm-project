2a10e795dc5baaf79a5e8f680260632947683549
Andrei Elovikov <andrei.elovikov@sifive.com>
2026-03-03 09:57:48 -0800

    Don't pass RecipeBuilder
    Legacy calls `setRecipe` on all processed recipes but really queries `getRecipe`
    for memory operations only, that we don't touch in the scalarization as that
    happens after all memory recipes has been processed.

--- llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanTransforms.cpp
@@ -6634,8 +6634,7 @@ void VPlanTransforms::makeMemOpWideningDecisions(
   }
 }
 
-void VPlanTransforms::makeScalarizationDecisions(
-    VPlan &Plan, VFRange &Range, VPRecipeBuilder &RecipeBuilder) {
+void VPlanTransforms::makeScalarizationDecisions(VPlan &Plan, VFRange &Range) {
   if (LoopVectorizationPlanner::getDecisionAndClampRange(
           [&](ElementCount VF) { return VF.isScalar(); }, Range))
     return;
@@ -6689,7 +6688,6 @@ void VPlanTransforms::makeScalarizationDecisions(
                                   *VPI, *VPI, VPI->getDebugLoc());
         Recipe->insertBefore(VPI);
         VPI->replaceAllUsesWith(Recipe);
-        RecipeBuilder.setRecipe(I, Recipe);
         VPI->eraseFromParent();
         continue;
       }
