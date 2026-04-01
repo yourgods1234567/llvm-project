921bac6a378e94f832fdd2d8a8ff00b9dab735c7
Daniel Donenfeld <ddonenfeld@nvidia.com>
2026-04-01 13:32:54 +0000

    Fix for LTO issue when multiple CU with mix of DebugDirectivesOnly and NoDebug

--- llvm/include/llvm/IR/DebugInfoMetadata.h
+++ llvm/include/llvm/IR/DebugInfoMetadata.h
@@ -2123,6 +2123,7 @@ public:
   DebugEmissionKind getEmissionKind() const {
     return (DebugEmissionKind)EmissionKind;
   }
+  bool isNoDebug() const { return EmissionKind == NoDebug; }
   bool isDebugDirectivesOnly() const {
     return EmissionKind == DebugDirectivesOnly;
   }
