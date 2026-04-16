widberg/llvm-project-widberg-extensions

cir bool to int

clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
@@ -1527,12 +1527,18 @@ RValue CIRGenFunction::emitBuiltinExpr(const GlobalDecl &gd, unsigned builtinID,
     Address resultPtr = emitPointerWithAlignment(resultArg);
 
     // Extend each operand to the encompassing type, if necessary.
-    if (left.getType() != encompassingCIRTy)
-      left =
-          builder.createCast(cir::CastKind::integral, left, encompassingCIRTy);
-    if (right.getType() != encompassingCIRTy)
-      right =
-          builder.createCast(cir::CastKind::integral, right, encompassingCIRTy);
+    if (left.getType() != encompassingCIRTy) {
+      auto kind = mlir::isa<cir::BoolType>(left.getType())
+                      ? cir::CastKind::bool_to_int
+                      : cir::CastKind::integral;
+      left = builder.createCast(kind, left, encompassingCIRTy);
+    }
+    if (right.getType() != encompassingCIRTy) {
+      auto kind = mlir::isa<cir::BoolType>(right.getType())
+                      ? cir::CastKind::bool_to_int
+                      : cir::CastKind::integral;
+      right = builder.createCast(kind, right, encompassingCIRTy);
+    }
 
     // Perform the operation on the extended values.
     cir::BinOpOverflowKind opKind;

yourgods1234567/llvm-project

clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
@@ -2019,10 +2019,18 @@ RValue CIRGenFunction::emitBuiltinExpr(const GlobalDecl &gd, unsigned builtinID,
     Address resultPtr = emitPointerWithAlignment(resultArg);
 
     // Extend each operand to the encompassing type, if necessary.
-    if (x.getType() != encompassingCIRTy)
-      x = builder.createCast(cir::CastKind::integral, x, encompassingCIRTy);
-    if (y.getType() != encompassingCIRTy)
-      y = builder.createCast(cir::CastKind::integral, y, encompassingCIRTy);
+    if (x.getType() != encompassingCIRTy) {
+      auto kind = mlir::isa<cir::BoolType>(x.getType())
+                      ? cir::CastKind::bool_to_int
+                      : cir::CastKind::integral;
+      x = builder.createCast(kind, x, encompassingCIRTy);
+    }
+    if (y.getType() != encompassingCIRTy) {
+      auto kind = mlir::isa<cir::BoolType>(y.getType())
+                      ? cir::CastKind::bool_to_int
+                      : cir::CastKind::integral;
+      y = builder.createCast(kind, y, encompassingCIRTy);
+    }
 
     // Perform the operation on the extended values.
     mlir::Location loc = getLoc(e->getSourceRange());