f0e48adea890071f140bfc47623725b260cedcbe
Evgeny Leviant <eleviant@accesssoftek.com>
2026-04-24 20:54:06 +0200

    Address review comments from @efriedma-quic and @nikic

--- llvm/lib/CodeGen/AtomicExpandPass.cpp
+++ llvm/lib/CodeGen/AtomicExpandPass.cpp
@@ -1682,7 +1682,9 @@ bool AtomicExpandImpl::expandAtomicCmpXchg(AtomicCmpXchgInst *CI) {
 }
 
 bool AtomicExpandImpl::isIdempotentRMW(AtomicRMWInst *RMWI) {
-  // TODO: Add floating point support.
+  if (RMWI->isVolatile())
+    return false;
+  // TODO: Add floating point support.  
   auto C = dyn_cast<ConstantInt>(RMWI->getValOperand());
   if (!C)
     return false;
